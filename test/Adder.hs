0377c5d99ffd2fe9f05b6964e66e7a5cb4c84eae
Conal Elliott <conal@conal.net>
2013-06-04 14:38:58 -0700

    test/Adder

---
+++ test/Adder.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wall -fno-warn-unused-imports #-}
+
+module Adder (halfAdd) where
+
+import Prelude hiding (and)
+
+-- import LambdaCCC.CCC
+import LambdaCCC.FunCCC
+
+halfAdd :: (Bool,Bool) -> (Bool,Bool)
+halfAdd p = (xor p, and p)
+
+-- halfAdd (a,b) = (xor (a,b), and (a,b))
+
+xor, and :: (Bool,Bool) :-> Bool
+
+xor (True,False) = True
+xor (False,True) = True
+xor _            = False
+
+and (True,True) = True
+and _           = False
