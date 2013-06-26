bc1e5c2ebbae706e8a668331a7a13e120d4d34c2
Conal Elliott <conal@conal.net>
2013-06-25 19:12:32 -0700

    tyHasTy reformulation

--- src/LambdaCCC/Ty.hs
+++ src/LambdaCCC/Ty.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeOperators, GADTs, KindSignatures, TypeSynonymInstances #-}
+{-# LANGUAGE ConstraintKinds #-}
 {-# OPTIONS_GHC -Wall #-}
 
 -- {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
@@ -16,7 +17,11 @@
 -- Typed types
 ----------------------------------------------------------------------
 
-module LambdaCCC.Ty (HasTy(..),Ty(..)) where
+module LambdaCCC.Ty
+  ( Ty(..),HasTy(..)
+  , HasTy2,HasTy3,HasTy4
+  , HasTyJt(..), tyHasTy, tyHasTy2
+  ) where
 
 import Control.Applicative (liftA2)
 
@@ -34,6 +39,7 @@ data Ty :: * -> * where
   IntT  :: Ty Int
   BoolT :: Ty Bool
   (:*)  :: Ty a -> Ty b -> Ty (a :*  b)
+  (:+)  :: Ty a -> Ty b -> Ty (a :+  b)
   (:=>) :: Ty a -> Ty b -> Ty (a :=> b)
 
 instance Show (Ty a) where
@@ -41,6 +47,7 @@ instance Show (Ty a) where
   showsPrec _ IntT      = showString "Int"
   showsPrec _ BoolT     = showString "Bool"
   showsPrec p (a :*  b) = showsOp2' ":*"  (7,AssocLeft ) p a b
+  showsPrec p (a :+  b) = showsOp2' ":+"  (6,AssocLeft ) p a b
   showsPrec p (a :=> b) = showsOp2' ":=>" (1,AssocRight) p a b
 
 instance IsTy Ty where
@@ -48,14 +55,36 @@ instance IsTy Ty where
   IntT      `tyEq` IntT        = Just Refl
   BoolT     `tyEq` BoolT       = Just Refl
   (a :*  b) `tyEq` (a' :*  b') = liftA2 liftEq2 (tyEq a a') (tyEq b b')
+  (a :+  b) `tyEq` (a' :+  b') = liftA2 liftEq2 (tyEq a a') (tyEq b b')
   (a :=> b) `tyEq` (a' :=> b') = liftA2 liftEq2 (tyEq a a') (tyEq b b')
   _         `tyEq` _           = Nothing
 
 -- | Synthesize a type
 class HasTy a where typ :: Ty a
 
+type HasTy2 a b     = (HasTy a, HasTy b)
+type HasTy3 a b c   = (HasTy2 a b, HasTy c)
+type HasTy4 a b c d = (HasTy2 a b, HasTy2 c d)
+
 instance HasTy Unit where typ = UnitT
 instance HasTy Int  where typ = IntT
 instance HasTy Bool where typ = BoolT
-instance (HasTy a, HasTy b) => HasTy (a :*  b) where typ = typ :*  typ
-instance (HasTy a, HasTy b) => HasTy (a :=> b) where typ = typ :=> typ
+instance HasTy2 a b => HasTy (a :*  b) where typ = typ :*  typ
+instance HasTy2 a b => HasTy (a :+  b) where typ = typ :+  typ
+instance HasTy2 a b => HasTy (a :=> b) where typ = typ :=> typ
+
+-- | Judgment (proof) that 'HasTy'
+data HasTyJt :: * -> * where
+  HasTy :: HasTy a => HasTyJt a
+
+-- | Proof of @'HasTy' a@ from @'Ty' a@
+tyHasTy :: Ty a -> HasTyJt a
+tyHasTy UnitT = HasTy
+tyHasTy IntT  = HasTy
+tyHasTy BoolT = HasTy
+tyHasTy (a :*  b) | (HasTy,HasTy) <- tyHasTy2 a b = HasTy
+tyHasTy (a :+  b) | (HasTy,HasTy) <- tyHasTy2 a b = HasTy
+tyHasTy (a :=> b) | (HasTy,HasTy) <- tyHasTy2 a b = HasTy
+
+tyHasTy2 :: Ty a -> Ty b -> (HasTyJt a,HasTyJt b)
+tyHasTy2 a b = (tyHasTy a,tyHasTy b)
