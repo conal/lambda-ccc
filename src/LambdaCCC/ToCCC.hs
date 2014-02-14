2ae586230421c24e235f8889f412f2337b670d8e
Conal Elliott <conal@conal.net>
2014-02-13 19:42:26 -0800

    dropped Ty

--- src/LambdaCCC/ToCCC.hs
+++ src/LambdaCCC/ToCCC.hs
@@ -23,41 +23,34 @@ import Data.Functor ((<$>))
 import Control.Monad (mplus)
 import Data.Maybe (fromMaybe)
 
-import Data.IsTy
 import Data.Proof.EQ
 
 import LambdaCCC.Misc
 import LambdaCCC.CCC
-import LambdaCCC.Ty
 import LambdaCCC.Lambda
 
 {--------------------------------------------------------------------
     Conversion
 --------------------------------------------------------------------}
 
--- -- | Rewrite a lambda expression via CCC combinators
 -- toCCC' :: E a -> (Unit :-> a)
--- toCCC' a | HasTy <- expHasTy a
---          = convert UnitPat a
+-- toCCC' = convert UnitPat
 
+-- | Rewrite a lambda expression via CCC combinators
 toCCC :: E (a :=> b) -> (a :-> b)
-toCCC e | (HasTy,HasTy) <- funTyHasTy (expTy e) = to' e
-
-to' :: HasTy2 a b => E (a :=> b) -> (a :-> b)
-to' (Lam p e) = convert p e
-to' e = to' (Lam vp (e :^ ve))
+toCCC (Lam p e) = convert p e
+toCCC e = toCCC (Lam vp (e :^ ve))
  where
    (vp,ve) = vars "ETA"
 
 -- | Convert @\ p -> e@ to CCC combinators
-convert :: forall a b. HasTy2 a b =>
-           Pat a -> E b -> (a :-> b)
-convert _ (ConstE o _) = Const o
+convert :: forall a b. Pat a -> E b -> (a :-> b)
+convert _ (ConstE o) = Const o
 convert k (Var v) = fromMaybe (error $ "convert: unbound variable: " ++ show v) $
                     convertVar v k
-convert k (u :^ v)   | HasTy <- tyHasTy (domTy (expTy u))
+convert k (u :^ v)
   = applyE @. (convert k u &&& convert k v)
-convert k (Lam p e)  | (HasTy,HasTy) <- tyHasTy2 (patTy p) (expTy e)
+convert k (Lam p e)
                      = curryE (convert (k :# p) e)
 -- convert k (Case (a,p) (b,q) ab) =
 --   (convert (k :# a) p ||| convert (k :# b) q) . distl . (Id &&& convert k ab)
@@ -66,24 +59,20 @@ convert k (Lam p e)  | (HasTy,HasTy) <- tyHasTy2 (patTy p) (expTy e)
 --   | (HasTy,HasTy) <- funTyHasTy (expTy f), HasTy <- tyHasTy (domTy (expTy g))
 --   = curryE ((uncurryE (convert k f) ||| uncurryE (convert k g)) @. distl)
 
-convert k (Either f g)
-  | (HasTy,HasTy) <- funTyHasTy (expTy f), HasTy <- tyHasTy (domTy (expTy g))
-  = curryE ((convert' f ||| convert' g) @. distl)
+convert k (Either f g) = curryE ((convert' f ||| convert' g) @. distl)
  where
-   convert' :: HasTy2 p q => E (p :=> q) -> (a :* p :-> q)
+   convert' :: E (p :=> q) -> (a :* p :-> q)
    convert' h = uncurryE (convert k h)
 
-
 -- Convert a variable in context
-convertVar :: forall b a. HasTy2 a b => V b -> Pat a -> Maybe (a :-> b)
+convertVar :: forall b a. V b -> Pat a -> Maybe (a :-> b)
 convertVar u = conv
  where
-   conv :: forall c. HasTy2 c b => Pat c -> Maybe (c :-> b)
-   conv (VarPat v) | Just Refl <- v `tyEq` u = Just Id
-                   | otherwise               = Nothing
+   conv :: forall c. Pat c -> Maybe (c :-> b)
+   conv (VarPat v) | Just Refl <- v `unsafeEq1` u = Just Id
+                   | otherwise                    = Nothing
    conv UnitPat  = Nothing
-   conv (p :# q) | (HasTy,HasTy) <- tyHasTy2 (patTy p) (patTy q)
-                 = ((@. Exr) <$> conv q) `mplus` ((@. Exl) <$> conv p)
+   conv (p :# q) = ((@. Exr) <$> conv q) `mplus` ((@. Exl) <$> conv p)
    conv (p :@ q) = conv q `mplus` conv p
 
 -- Alternatively,
