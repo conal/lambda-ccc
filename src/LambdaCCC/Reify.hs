8ea54bd22a192ef3e00138f71e0c2029aa22b913
Conal Elliott <conal@conal.net>
2014-04-18 19:07:20 -0700

    OnlyLifted ; "Translate" --> "Transform", reifyMethod

--- src/LambdaCCC/Reify.hs
+++ src/LambdaCCC/Reify.hs
@@ -5,9 +5,7 @@
 {-# LANGUAGE Rank2Types #-}
 {-# OPTIONS_GHC -Wall #-}
 
--- TODO: Restore the following pragmas
-
--- {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
+{-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
 -- {-# OPTIONS_GHC -fno-warn-unused-binds   #-} -- TEMP
 
 ----------------------------------------------------------------------
@@ -22,6 +20,8 @@
 -- Reify a Core expression into GADT
 ----------------------------------------------------------------------
 
+#define OnlyLifted
+
 module LambdaCCC.Reify (plugin) where
 
 import Data.Functor ((<$>))
@@ -35,7 +35,7 @@ import HERMIT.GHC hiding (mkStringExpr)
 import HERMIT.Kure -- hiding (apply)
 -- Note that HERMIT.Dictionary re-exports HERMIT.Dictionary.*
 import HERMIT.Dictionary
-  ( observeR,callNameT
+  ( observeR, callT, callNameT
   , rulesR,inlineR,simplifyR,letFloatLetR,letFloatTopR,letElimR
   , betaReduceR, letNonRecSubstSafeR
   -- , unshadowR   -- May need this one later
@@ -45,9 +45,12 @@ import HERMIT.Plugin (hermitPlugin,phase,interactive)
 import LambdaCCC.Misc (Unop,(<~))
 
 import TypeEncode.Plugin
-  ( apps', ReExpr ,ReCore, TranslateU, findTyConT
+  ( apps', callSplitT, callNameSplitT, unCall1
+  , ReExpr ,ReCore, TransformU, findTyConT
+#ifdef OnlyLifted
   , liftedKind, unliftedKind
-  , encodeTypesR )
+#endif
+  , encodeOf, reConstructR, reCaseR )
 import qualified TypeEncode.Plugin as Enc
 
 {--------------------------------------------------------------------
@@ -85,14 +88,14 @@ observeR' :: InCoreTC t => String -> RewriteH t
 observeR' | observing = observeR
           | otherwise = const idR
 
-tries :: (InCoreTC a, InCoreTC t) => [(String,TranslateH a t)] -> TranslateH a t
-tries = foldr (<+) (observeR' "Unhandled" >>> fail "unhandled")
+tries :: (InCoreTC a, InCoreTC t) => [(String,TransformH a t)] -> TransformH a t
+tries = foldr (<+) ({- observeR' "Unhandled" >>> -} fail "unhandled")
       . map (uncurry labeled)
 
-labeled :: InCoreTC t => String -> Unop (TranslateH a t)
+labeled :: InCoreTC t => String -> Unop (TransformH a t)
 labeled label = (>>> observeR' label)
 
--- mkVarName :: MonadThings m => Translate c m Var (CoreExpr,Type)
+-- mkVarName :: MonadThings m => Transform c m Var (CoreExpr,Type)
 -- mkVarName = contextfreeT (mkStringExpr . uqName . varName) &&& arr varType
 
 varLitE :: Var -> CoreExpr
@@ -117,6 +120,9 @@ typeEtaLong = readerT $ \ e ->
 simplifyE :: ReExpr
 simplifyE = extractR simplifyR
 
+anytdE :: Unop ReExpr
+anytdE r = extractR (anytdR (promoteR r :: ReCore))
+
 -- TODO: Try rewriting more gracefully, testing isForAllTy first and
 -- maybeEtaExpandR
 
@@ -130,7 +136,7 @@ isAppTy :: CoreExpr -> Bool
 isAppTy (App _ (Type _)) = True
 isAppTy _                = False
 
-letFloatToProg :: TranslateH CoreBind CoreProg
+letFloatToProg :: TransformH CoreBind CoreProg
 letFloatToProg = arr (flip ProgCons ProgNil) >>> tryR letFloatTopR
 
 concatProgs :: [CoreProg] -> CoreProg
@@ -143,18 +149,25 @@ concatProgs = bindsToProg . concatMap progToBinds
 lamName :: Unop String
 lamName = ("LambdaCCC.Lambda." ++)
 
--- findIdE :: String -> TranslateH a Id
+-- findIdE :: String -> TransformH a Id
 -- findIdE = findIdT . lamName
 
-findTyConE :: String -> TranslateH a TyCon
+findTyConE :: String -> TransformH a TyCon
 findTyConE = findTyConT . lamName
 
-appsE :: String -> [Type] -> [CoreExpr] -> TranslateU CoreExpr
+appsE :: String -> [Type] -> [CoreExpr] -> TransformU CoreExpr
 appsE = apps' . lamName
 
+-- | Uncall a named function
+unCallE1 :: String -> ReExpr
+unCallE1 = unCall1 . lamName
+
 -- A handy form for composition via <=<
-appsE1 :: String -> [Type] -> CoreExpr -> TranslateU CoreExpr
+appsE1 :: String -> [Type] -> CoreExpr -> TransformU CoreExpr
 appsE1 str ts e = appsE str ts [e]
+
+-- callNameLam :: String -> TransformH CoreExpr (CoreExpr, [CoreExpr])
+-- callNameLam = callNameT . lamName
 
 -- Some names
 
@@ -171,24 +184,31 @@ epS :: String
 epS = "EP"
 
 -- t --> EP t
-epOf :: Type -> TranslateH a Type
-epOf t = do epTC <- findTyConE epS
-            return (TyConApp epTC [t])
+mkEP :: TransformH a Type
+mkEP = do epTC <- findTyConE epS
+          return (TyConApp epTC [])
+
+-- t --> EP t
+epOf :: Type -> TransformH a Type
+epOf t = flip mkAppTy t <$> mkEP
+
+-- epOf t = do epTC <- findTyConE epS
+--             return (TyConApp epTC [t])
 
 -- reify u --> u
 unReify :: ReExpr
-unReify = do (_reifyE, [Type _, arg]) <- callNameLam reifyS
-             return arg
+unReify = unCallE1 reifyS
 -- eval e --> e
 unEval :: ReExpr
-unEval = do (_evalE, [Type _, body]) <- callNameLam evalS
-            return body
+unEval = unCallE1 evalS
 
 -- reify (eval e) --> e
 reifyEval :: ReExpr
 reifyEval = unReify >>> unEval
 
-reifyOf :: CoreExpr -> TranslateU CoreExpr
+#ifdef OnlyLifted
+
+reifyOf :: CoreExpr -> TransformU CoreExpr
 reifyOf e = do guardMsg (not (unliftedKind ki))
                  "reifyOf: no unlifted values (with type of kind #)"
                guardMsg (liftedKind ki)
@@ -203,7 +223,14 @@ kindStr :: Kind -> String
 kindStr (TyConApp tc _) = "TyConApp " ++ uqName (tyConName tc) ++ "..."
 kindStr _               = "not a TyConApp" -- TODO: more detail here if needed
 
-evalOf :: CoreExpr -> TranslateU CoreExpr
+#else
+
+reifyOf :: CoreExpr -> TransformU CoreExpr
+reifyOf e = appsE reifyS [exprType e] [e]
+
+#endif
+
+evalOf :: CoreExpr -> TransformU CoreExpr
 evalOf e = appsE evalS [dropEP (exprType e)] [e]
 
 isEP :: Type -> Bool
@@ -230,15 +257,17 @@ reifyApp = do App u v <- unReify
               v' <- reifyOf v
               appsE "appP" [b,a] [u', v'] -- note b,a
 
+-- TODO: Use arr instead of (constT (return ...))
 -- TODO: refactor so we unReify once and then try variations
 
-varEval :: Var -> TranslateU CoreExpr
+varEval :: Var -> TransformU CoreExpr
 varEval v = (evalOf <=< appsE1 varPS [varType v]) (varLitE v)
 
-varSubst :: [Var] -> TranslateU (Unop CoreExpr)
+varSubst :: [Var] -> TransformU (Unop CoreExpr)
 varSubst vs = do vs' <- mapM varEval vs
                  return (subst (vs `zip` vs'))
 
+-- | reify (\ x -> e)  -->  lamv x' (reify (e[x := eval (var x')]))
 reifyLam :: ReExpr
 reifyLam = do Lam v e <- unReify
               guardMsg (not (isTyVar v)) "reifyLam: doesn't handle type lambda"
@@ -274,11 +303,14 @@ reifyPolyLet = unReify >>>
 -- one level. Typically (always?) the "foo = eval foo_reified" definition gets
 -- inlined and then eliminated by the letElimR in reifyMisc.
 
-monoLetToRedex :: ReExpr
-monoLetToRedex = do Let (NonRec v@(isForAllTy . varType -> False) rhs) body <- idR
-                    return (Lam v body `App` rhs)
+-- | Turn a monomorphic let into a beta-redex.
+reifyMonoLet :: ReExpr
+reifyMonoLet =
+  inReify $
+    do Let (NonRec v@(isForAllTy . varType -> False) rhs) body <- idR
+       return (Lam v body `App` rhs)
 
--- TODO: Perhaps combine reifyPolyLet and monoLetToRedex into reifyLet
+-- TODO: Perhaps combine reifyPolyLet and reifyMonoLet into reifyLet
 
 #define SplitEval
 
@@ -337,8 +369,6 @@ reifyModGuts = modGutsR reifyProg
 
 -- TODO: How to float the local bindings as well?
 
--- TODO: Use arr instead of (consT (return (f ...)))
-
 -- Inline if doing so yields an eval
 inlineEval :: ReExpr
 inlineEval = inAppTys (inlineR >>> acceptR isTyLamsEval) >>> tryR simplifyE
@@ -358,13 +388,13 @@ reifyInline :: ReExpr
 reifyInline = inReify inlineNonE
 -- reifyInline = inReify inlineEval
 
-reifyEncode :: ReExpr
-reifyEncode = inReify encodeTypesR
+-- reifyEncode :: ReExpr
+-- reifyEncode = inReify encodeTypesR
 
 reifyRuleNames :: [String]
 reifyRuleNames = map ("reify/" ++)
   [ "not","(&&)","(||)","xor","(+)","exl","exr","pair","inl","inr"
-  , "if","false","decode","encode","true","if-bool","if-pair" ]
+  , "if","encode","decode","()","false","true","if-bool","if-pair" ]
 
 -- or: words "not (&&) (||) xor ..."
 
@@ -373,17 +403,80 @@ reifyRuleNames = map ("reify/" ++)
 -- Keep for now, to help us see that whether reify applications vanish.
 
 reifyRules :: ReExpr
-reifyRules = rulesR reifyRuleNames >>> tryR simplifyE
+reifyRules = (rulesR reifyRuleNames >>> tryR simplifyE)
+#ifndef OnlyLifted
+             <+ reifyCodeF
+#endif
 
 -- reifyRules = rulesR reifyRuleNames >>> tryR (extractR tidy)
 --  where
 --    tidy :: ReCore
 --    tidy = anybuR (promoteR (betaReduceR >>> letNonRecSubstSafeR))
 
+-- -- eval e |> co  -->  eval (e |> co').
+-- -- Since `eval :: E p a -> a` and `co :: a ~ b`, `co' :: E p a ~ E p b`.
+-- evalCast :: ReExpr
+-- evalCast = (do ep <- mkEP
+--                castAllR unEval (arr (mkAppCo (mkSubCo (Refl Nominal ep))))) >>>
+--            evalR              
+
+-- reify (I# n) --> intL (I# n)
+reifyIntLit :: ReExpr
+reifyIntLit = unReify >>> do _ <- callNameT "I#"
+                             e <- idR
+                             apps' "intL" [] [e]
+
+-- TODO: Why does apps' work here in place of appsE?
+-- Maybe simply because the examples import LambdaCCC.Lambda unqualified.
+
+#ifndef OnlyLifted
+
+reifyDecodeF :: ReExpr
+reifyDecodeF = unReify >>>
+               do -- (_,[Type a,Type b]) <- callNameT "TypeDecode.Encode.decodeF"
+                  (Var f,[Type a,Type b]) <- callT
+                  guardMsg (uqVarName f == "decodeF") "oops -- not decodeF"
+                  apps' "LambdaCCC.Prim.DecodeP" [a,b] [] >>= appsE1 "kPrimEP" [FunTy a b]
+
+reifyEncodeF :: ReExpr
+reifyEncodeF = unReify >>>
+               do -- (_,[Type a,Type b]) <- callNameT "TypeDecode.Encode.encodeF"
+                  (Var f,[Type a,Type b]) <- callT
+                  guardMsg (uqVarName f == "encodeF") "oops -- not encodeF"
+                  apps' "LambdaCCC.Prim.EncodeP" [a,b] [] >>= appsE1 "kPrimEP" [FunTy a b]
+
+reifyCodeF :: ReExpr
+reifyCodeF = reifyEncodeF <+ reifyDecodeF
+
+#endif
+
+-- TODO: refactor
+
+-- reify (e |> co)  -->  reify (encode e)
+reifyCast :: ReExpr
+reifyCast = inReify $ 
+              do e'@(Cast e _) <- idR
+                 let ty  = exprType e
+                     ty' = exprType e'
+                 encodeOf ty ty' e
+
+-- TODO: rework reifyCast so that we can recognize and and eliminate composed
+-- inverses.
+
 typeBetaReduceR :: ReExpr
 typeBetaReduceR = do (isTyLam -> True) `App` _ <- idR
                      betaReduceR >>> letNonRecSubstSafeR
 
+-- Given reifyEP (m d), if m is a variable and d is a dictionary, then anytdR inline >>> simplify.
+reifyMethod :: ReExpr
+reifyMethod = inReify $
+              do (_, _,[d]) <- callSplitT
+                 guardMsg (isDictTy (exprType d)) "non-dictionary"
+                 anytdE inlineR >>> simplifyE
+
+-- Note: Given reify (m d a .. z), we'll use reifyApp to whittle down to reify (m d)
+
+-- reify of case on 0-tuple or 2-tuple
 reifyTupCase :: ReExpr
 reifyTupCase =
   do Case scrut@(exprType -> scrutT) wild bodyT [branch] <- unReify
@@ -394,7 +487,7 @@ reifyTupCase =
    -- Reify a case alternative, yielding a reified pattern and a reified
    -- alternative body (RHS). Only unit and pair patterns. Others are
    -- transformed away in the type-encode plugin.
-   reifyBranch :: Var -> CoreAlt -> TranslateU (CoreExpr,CoreExpr)
+   reifyBranch :: Var -> CoreAlt -> TransformU (CoreExpr,CoreExpr)
    reifyBranch wild (DataAlt ( isTupleTyCon . dataConTyCon -> True)
                              , vars, rhs ) =
      do guardMsg (length vars `elem` [0,2])
@@ -413,25 +506,13 @@ reifyTupCase =
         rhs'  <- reifyOf (sub rhs)
         return (pat', rhs')
     where
-      varPatT :: Var -> TranslateU CoreExpr
+      varPatT :: Var -> TransformU CoreExpr
       varPatT v = appsE varPatS [varType v] [varLitE v]
    reifyBranch _ _ = fail "reifyBranch: Only handles pair patterns so far."
 
 reifyEither :: ReExpr
 
 #if 1
-
-splitTysVals :: [Expr b] -> ([Type], [Expr b])
-splitTysVals (Type ty : rest) = first (ty :) (splitTysVals rest)
-splitTysVals rest             = ([],rest)
-
-callNameSplitT :: MonadCatch m => String
-               -> Translate c m CoreExpr (CoreExpr, [Type], [Expr CoreBndr])
-callNameSplitT name = do (f,args) <- callNameT name
-                         let (tyArgs,valArgs) = splitTysVals args
-                         return (f,tyArgs,valArgs)
-
--- TODO: Move splitTysVals and callNameSplitT
 
 reifyEither = unReify >>>
               do (_either, tys, funs@[_,_]) <- callNameSplitT "either"
@@ -446,11 +527,11 @@ reifyEither = unReify >>>
 
 #else
 
-eitherTy :: Type -> Type -> TranslateU Type
+eitherTy :: Type -> Type -> TransformU Type
 eitherTy a b = do tc <- findTyConT "Either"
                   return (TyConApp tc [a,b])
 
-unEitherTy :: Type -> TranslateU (Type,Type)
+unEitherTy :: Type -> TransformU (Type,Type)
 unEitherTy (TyConApp tc [a,b]) =
   do etc <- findTyConT "Either"
      guardMsg (tyConName tc == tyConName etc)
@@ -472,7 +553,7 @@ reifyEither =
      scrut'  <- reifyOf scrut
      appsE "appP" [bodyT,t] [e,scrut']
  where
-   reifyBranch :: Var -> CoreAlt -> TranslateU CoreExpr
+   reifyBranch :: Var -> CoreAlt -> TransformU CoreExpr
    reifyBranch _wild (DataAlt _, [var], rhs) = reifyOf (Lam var rhs)
    reifyBranch _ _ = error "reifyEither: bad branch"
 
@@ -481,30 +562,44 @@ reifyEither =
 
 #endif
 
+-- | reify (case scrut of wild t { _ -> body })
+--    --> reify (let wild = scrut in body)
+-- May be followed by let-elimination.
+-- Note: does not capture GHC's intent to reduce scrut to WHNF.
+reifyCaseWild :: ReExpr
+reifyCaseWild = inReify $
+                  do Case scrut wild _bodyTy [(DEFAULT,[],body)] <- idR
+                     return $ Let (NonRec wild scrut) body
+
+reifyConstruct :: ReExpr
+reifyConstruct = inReify reConstructR
+
+reifyCase :: ReExpr
+reifyCase = inReify reCaseR
+
 reifyMisc :: ReExpr
-reifyMisc = tries [ ("reifyEval"      , reifyEval)
-                  , ("reifyEither"    , reifyEither)  -- before App
-                  , ("reifyEncode"    , reifyEncode)  -- before App
+reifyMisc = tries [ ("reifyRules"     , reifyRules)     -- before App
+                  , ("reifyEval"      , reifyEval)      -- ''
+                  , ("reifyEither"    , reifyEither)    -- ''
+                  , ("reifyConstruct" , reifyConstruct) -- ''
+                  , ("reifyMethod"    , reifyMethod)    -- ''
+                  , ("reifyCase"      , reifyCase)
+                  , ("reifyCaseWild"  , reifyCaseWild)
                   , ("reifyApp"       , reifyApp)
                   , ("reifyLam"       , reifyLam)
+                  , ("reifyMonoLet"   , reifyMonoLet)
                   , ("reifyPolyLet"   , reifyPolyLet)
                   , ("reifyTupCase"   , reifyTupCase)
-                  , ("reifyRules"     , reifyRules)
                   , ("reifyInline"    , reifyInline)
+                  , ("reifyCast"      , reifyCast)
+                  , ("reifyIntLit"    , reifyIntLit)
                   -- Helpers:
-                  , ("monoLetToRedex" , monoLetToRedex)
                   , ("typeBetaReduceR", typeBetaReduceR)
                   , ("letElim"        , letElimR)  -- Note
-                  -- Data type encoding
-                  -- , ("encodeTypesR"   , encodeTypesR)
                   ]
-
 
 -- Note: letElim is handy with reifyPolyLet to eliminate the "foo = eval
 -- foo_reified", which is usually inaccessible.
-
-callNameLam :: String -> TranslateH CoreExpr (CoreExpr, [CoreExpr])
-callNameLam = callNameT . lamName
 
 {--------------------------------------------------------------------
     Plugin
@@ -535,17 +630,28 @@ externals =
     , externC "reify-inline" reifyInline "inline names where reified"
     , externC "reify-it" reifyR "apply reify"
     , externC "eval-it" evalR "apply eval"
-    , externC "reify-let" reifyPolyLet "reify polymorphic let"
     , externC "reify-app" reifyApp "reify (u v) --> reify u `appP` reify v"
-    , externC "let-to-redex" monoLetToRedex "monomorphic let to redex"
+    , externC "reify-lam" reifyLam
+        "reify (\\ x -> e) --> lamv x' (reify (e[x := eval (var x')]))"
+    , externC "reify-mono-let" reifyMonoLet "reify monomorphic let"
+    , externC "reify-poly-let" reifyPolyLet "reify polymorphic let"
+    , externC "reify-case-wild" reifyCaseWild "reify a evaluation-only case"
+    , externC "reify-construct" reifyApp "re-construct under reify"
+    , externC "reify-case" reifyCase "re-case under reify"
+    , externC "reify-method" reifyMethod "reify of a method application"
+    , externC "reify-cast" reifyCast "reify a cast"
+    , externC "reify-int-literal" reifyIntLit "reify an Int literal"
     , externC "reify-eval" reifyEval "reify eval"
     , externC "reify-tup-case" reifyTupCase "reify case with unit or pair pattern"
     , externC "reify-module" reifyModGuts "reify all top-level definitions"
     , externC "inline-eval" inlineEval "inline to an eval"
     , externC "type-eta-long" typeEtaLong "type-eta-long form"
     , externC "reify-poly-let" reifyPolyLet "reify polymorphic 'let' expression"
-    , externC "reify-encode" reifyEncode "type-encode under reify"
-    , externC "encode-types" encodeTypesR
-       "encode case expressions and constructor applications"
+--     , externC "reify-encode" reifyEncode "type-encode under reify"
+--     , externC "encode-types" encodeTypesR
+--        "encode case expressions and constructor applications"
+#ifndef OnlyLifted
+    , externC "reify-code" reifyCodeF "manual rewrites for reifying encodeF & decodeF"
+#endif
     ] ++ Enc.externals
 
