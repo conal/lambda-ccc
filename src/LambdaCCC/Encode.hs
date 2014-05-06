bead118ebc9d76249bfb45e4860a4d4563e3cc20
Conal Elliott <conal@conal.net>
2014-05-06 13:20:18 -0700

    INLINE pragmas ; LANGUAGE pragma trimming

--- src/LambdaCCC/Encode.hs
+++ src/LambdaCCC/Encode.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE TypeFamilies, TypeOperators, DataKinds, GADTs #-}
-{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
-{-# LANGUAGE FlexibleInstances, FlexibleContexts, EmptyDataDecls #-}
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeFamilies, TypeOperators #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}   -- See below
+{-# LANGUAGE CPP #-}
 
 {-# OPTIONS_GHC -Wall #-}
 
@@ -20,7 +20,7 @@
 -- Statically typed lambda expressions
 ----------------------------------------------------------------------
 
-module LambdaCCC.Encode where
+module LambdaCCC.Encode (Encodable(..)) where
 
 -- import Prelude hiding (id,(.))
 import Data.Foldable
@@ -34,15 +34,7 @@ import TypeUnary.Vec
 import Circat.Pair
 import Circat.RTree
 
-type Unop  a = a -> a
-type Binop a = a -> Unop a
-
-infixl 6 :+
-infixl 7 :*
-
-type Unit  = ()
-type (:*)  = (,)
-type (:+)  = Either
+import LambdaCCC.Misc (Unop,Binop,Unit,(:+),(:*))
 
 infixr 1 -->
 -- | Add pre- and post processing
@@ -52,85 +44,113 @@ infixr 1 -->
 -- (-->) :: Category k =>
 --          (a' `k` a) -> (b `k` b') -> ((a `k` b) -> (a' `k` b'))
 
-
 class Encodable a where
   type Encode a
   encode :: a -> Encode a
   decode :: Encode a -> a
 
-instance Encodable () where
-  type Encode () = ()
-  encode = id
-  decode = id
-
 instance (Encodable a, Encodable b) => Encodable (a :* b) where
   type Encode (a :* b) = Encode a :* Encode b
   encode = encode *** encode
   decode = decode *** decode
+  {-# INLINE encode #-}
+  {-# INLINE decode #-}
 
 instance (Encodable a, Encodable b) => Encodable (a :+ b) where
   type Encode (a :+ b) = Encode a :+ Encode b
   encode = encode +++ encode
   decode = decode +++ decode
+  {-# INLINE encode #-}
+  {-# INLINE decode #-}
 
 instance (Encodable a, Encodable b) => Encodable (a -> b) where
   type Encode (a -> b) = Encode a -> Encode b
   encode = decode --> encode
   decode = encode --> decode
+  {-# INLINE encode #-}
+  {-# INLINE decode #-}
+
+#define PrimEncode(t) \
+ instance Encodable (t) where \
+   { type Encode (t) = t ; encode = id ; decode = id; \
+     {-# INLINE encode #-}; {-# INLINE decode #-}; \
+   }
+
+PrimEncode(Unit)
+PrimEncode(Bool)
+PrimEncode(Int)
 
 -- instance Encodable Bool where
---   { type Encode Bool = Bool ; encode = id ; decode = id }
-
-instance Encodable Bool where
-  type Encode Bool = () :+ ()
-  encode False = Left ()
-  encode True  = Right ()
-  decode (Left  ()) = False
-  decode (Right ()) = True
+--   type Encode Bool = () :+ ()
+--   encode False = Left ()
+--   encode True  = Right ()
+--   decode (Left  ()) = False
+--   decode (Right ()) = True
 
 -- instance Encodable Bool where
 --   type Encode Bool = () :+ ()
 --   encode = bool (Left ()) (Right ())
 --   decode = const False ||| const True
 
-bool :: a -> a -> Bool -> a
-bool t e i = if i then t else e
+-- bool :: a -> a -> Bool -> a
+-- bool t e i = if i then t else e
 
 {--------------------------------------------------------------------
     Library types
 --------------------------------------------------------------------}
 
-instance Encodable Any where
-  { type Encode Any = Encode Bool ; encode = encode . getAny ; decode = Any . decode }
+#define NewtypeEncode(n,o,wrap,unwrap) \
+  instance Encodable (n) where \
+    { type Encode (n) = Encode (o) ; encode = encode . unwrap ; decode = wrap . decode ; \
+      {-# INLINE encode #-}; {-# INLINE decode #-}; \
+    }
 
-instance Encodable All where
-  { type Encode All = Encode Bool ; encode = encode . getAll ; decode = All . decode }
-
+NewtypeEncode(Any,Bool,Any,getAny)
+NewtypeEncode(All,Bool,All,getAll)
 -- etc
 
+--     Application is no smaller than the instance head
+--       in the type family application: Encode Bool
+--     (Use UndecidableInstances to permit this)
+--     In the type instance declaration for ‘Encode’
+--     In the instance declaration for ‘Encodable (Any)’
+
+
 -- TODO: Can we get some help from the Newtype class?
+
+-- Sized types
 
 instance Encodable a => Encodable (Pair a) where
   type Encode (Pair a) = Encode (a :* a)
   encode (a :# a') = (encode a, encode a')
   decode (b,b') = decode b :# decode b'
+  {-# INLINE encode #-}
+  {-# INLINE decode #-}
 
 instance Encodable (Vec Z a) where
   type Encode (Vec Z a) = Unit
   encode ZVec = ()
   decode () = ZVec
+  {-# INLINE encode #-}
+  {-# INLINE decode #-}
 
 instance (Encodable a, Encodable (Vec n a)) => Encodable (Vec (S n) a) where
   type Encode (Vec (S n) a) = Encode (a :* Vec n a)
   encode (a :< as) = (encode a, encode as)
   decode (b,bs) = decode b :< decode bs
+  {-# INLINE encode #-}
+  {-# INLINE decode #-}
 
 instance Encodable (Tree Z a) where
   type Encode (Tree Z a) = a
   encode (L a) = a
   decode a = L a
+  {-# INLINE encode #-}
+  {-# INLINE decode #-}
 
 instance Encodable (Tree n a) => Encodable (Tree (S n) a) where
   type Encode (Tree (S n) a) = Encode (Pair (Tree n a))
   encode (B ts) = encode ts
   decode x = B (decode x)
+  {-# INLINE encode #-}
+  {-# INLINE decode #-}
