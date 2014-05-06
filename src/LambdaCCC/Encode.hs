dab6e774ec1e95dc0e615553389c35df021a3dca
Conal Elliott <conal@conal.net>
2014-05-06 13:35:25 -0700

    EncTy, EEncTy macros

--- src/LambdaCCC/Encode.hs
+++ src/LambdaCCC/Encode.hs
@@ -47,36 +47,54 @@ class Encodable a where
   encode :: a -> Encode a
   decode :: Encode a -> a
 
+#define EncTy(n,o) type Encode (n) = o ; INS
+
 instance (Encodable a, Encodable b) => Encodable (a :* b) where
-  type Encode (a :* b) = Encode a :* Encode b ; INS
+  EncTy(a :* b, Encode a :* Encode b)
   encode = encode *** encode
   decode = decode *** decode
 
 instance (Encodable a, Encodable b) => Encodable (a :+ b) where
-  type Encode (a :+ b) = Encode a :+ Encode b ; INS
+  EncTy (a :+ b, Encode a :+ Encode b)
   encode = encode +++ encode
   decode = decode +++ decode
 
 instance (Encodable a, Encodable b) => Encodable (a -> b) where
-  type Encode (a -> b) = Encode a -> Encode b ; INS
+  EncTy (a -> b, Encode a -> Encode b)
   encode = decode --> encode
   decode = encode --> decode
 
 #define PrimEncode(t) \
- instance Encodable (t) where \
-   { type Encode (t) = t ; INS ; encode = id ; decode = id }
+ instance Encodable (t) where { EncTy(t,t) ; encode = id ; decode = id }
 
 PrimEncode(Unit)
 PrimEncode(Bool)
 PrimEncode(Int)
 
+-- instance Encodable Bool where
+--   type Encode Bool = () :+ () ; INS
+--   encode False = Left ()
+--   encode True  = Right ()
+--   decode (Left  ()) = False
+--   decode (Right ()) = True
+
+-- instance Encodable Bool where
+--   type Encode Bool = () :+ () ; INS
+--   encode = bool (Left ()) (Right ())
+--   decode = const False ||| const True
+
+-- bool :: a -> a -> Bool -> a
+-- bool t e i = if i then t else e
+
 {--------------------------------------------------------------------
     Library types
 --------------------------------------------------------------------}
 
+#define EEncTy(n,o) EncTy(n,Encode(o))
+
 #define NewtypeEncode(n,o,wrap,unwrap) \
   instance Encodable (n) where \
-    { type Encode (n) = Encode (o) ; INS ; encode = encode . unwrap ; decode = wrap . decode }
+    { EEncTy (n,o) ; encode = encode . unwrap ; decode = wrap . decode }
 
 NewtypeEncode(Any,Bool,Any,getAny)
 NewtypeEncode(All,Bool,All,getAll)
@@ -94,26 +112,26 @@ NewtypeEncode(All,Bool,All,getAll)
 -- Sized types
 
 instance Encodable a => Encodable (Pair a) where
-  type Encode (Pair a) = Encode (a :* a) ; INS
+  EEncTy (Pair a, a :* a)
   encode (a :# a') = (encode a, encode a')
   decode (b,b') = decode b :# decode b'
 
 instance Encodable (Vec Z a) where
-  type Encode (Vec Z a) = Unit ; INS
+  EEncTy (Vec Z a, Unit)
   encode ZVec = ()
   decode () = ZVec
 
 instance (Encodable a, Encodable (Vec n a)) => Encodable (Vec (S n) a) where
-  type Encode (Vec (S n) a) = Encode (a :* Vec n a) ; INS
+  EEncTy (Vec (S n) a, a :* Vec n a)
   encode (a :< as) = (encode a, encode as)
   decode (b,bs) = decode b :< decode bs
 
-instance Encodable (Tree Z a) where
-  type Encode (Tree Z a) = a ; INS
-  encode (L a) = a
-  decode a = L a
+instance Encodable a => Encodable (Tree Z a) where
+  EEncTy (Tree Z a, a)
+  encode (L a) = encode a
+  decode a = L (decode a)
 
 instance Encodable (Tree n a) => Encodable (Tree (S n) a) where
-  type Encode (Tree (S n) a) = Encode (Pair (Tree n a)) ; INS
+  EEncTy (Tree (S n) a, Pair (Tree n a))
   encode (B ts) = encode ts
   decode x = B (decode x)
