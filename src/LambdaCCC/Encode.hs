c6a67efa7a4a97f7d4ba758cb06df646cd3aabe4
Conal Elliott <conal@conal.net>
2014-05-06 13:45:30 -0700

    "NewtypeEncode" --> "RepEncode"; use for Pair, Vec, Tree

--- src/LambdaCCC/Encode.hs
+++ src/LambdaCCC/Encode.hs
@@ -26,9 +26,9 @@ import Control.Arrow ((***),(+++))
 import Data.Monoid (Any(..),All(..))
 
 import TypeUnary.TyNat (Z,S)
-import TypeUnary.Vec (Vec(..))
-import Circat.Pair (Pair(..))
-import Circat.RTree (Tree(..))
+import TypeUnary.Vec (Vec(..),unConsV)
+import Circat.Pair (Pair(..),toP,fromP)
+import Circat.RTree (Tree(..),toL,unL,toB,unB)
 
 import LambdaCCC.Misc (Unit,(:+),(:*))
 
@@ -40,6 +40,7 @@ infixr 1 -->
 -- (-->) :: Category k =>
 --          (a' `k` a) -> (b `k` b') -> ((a `k` b) -> (a' `k` b'))
 
+-- Inlining!
 #define INS {-# INLINE encode #-} ; {-# INLINE decode #-}
 
 class Encodable a where
@@ -72,19 +73,11 @@ PrimEncode(Bool)
 PrimEncode(Int)
 
 -- instance Encodable Bool where
---   type Encode Bool = () :+ () ; INS
+--   EncTy (Bool,() :+ ())
 --   encode False = Left ()
 --   encode True  = Right ()
 --   decode (Left  ()) = False
 --   decode (Right ()) = True
-
--- instance Encodable Bool where
---   type Encode Bool = () :+ () ; INS
---   encode = bool (Left ()) (Right ())
---   decode = const False ||| const True
-
--- bool :: a -> a -> Bool -> a
--- bool t e i = if i then t else e
 
 {--------------------------------------------------------------------
     Library types
@@ -92,12 +85,12 @@ PrimEncode(Int)
 
 #define EEncTy(n,o) EncTy(n,Encode(o))
 
-#define NewtypeEncode(n,o,wrap,unwrap) \
-  instance Encodable (n) where \
-    { EEncTy (n,o) ; encode = encode . unwrap ; decode = wrap . decode }
+#define RepEncode(n,o,wrap,unwrap) \
+  instance Encodable (o) => Encodable (n) where \
+    { EEncTy (n,o) ; encode = encode . (unwrap) ; decode = (wrap) . decode }
 
-NewtypeEncode(Any,Bool,Any,getAny)
-NewtypeEncode(All,Bool,All,getAll)
+RepEncode(Any,Bool,Any,getAny)
+RepEncode(All,Bool,All,getAll)
 -- etc
 
 --     Application is no smaller than the instance head
@@ -106,32 +99,12 @@ NewtypeEncode(All,Bool,All,getAll)
 --     In the type instance declaration for ‘Encode’
 --     In the instance declaration for ‘Encodable (Any)’
 
-
 -- TODO: Can we get some help from the Newtype class?
 
--- Sized types
+RepEncode(Pair a, a :* a, toP, fromP)
 
-instance Encodable a => Encodable (Pair a) where
-  EEncTy (Pair a, a :* a)
-  encode (a :# a') = (encode a, encode a')
-  decode (b,b') = decode b :# decode b'
+RepEncode(Vec Z a, Unit, \ () -> ZVec, \ ZVec -> ())
+RepEncode(Vec (S n) a, a :* Vec n a, uncurry (:<), unConsV)
 
-instance Encodable (Vec Z a) where
-  EEncTy (Vec Z a, Unit)
-  encode ZVec = ()
-  decode () = ZVec
-
-instance (Encodable a, Encodable (Vec n a)) => Encodable (Vec (S n) a) where
-  EEncTy (Vec (S n) a, a :* Vec n a)
-  encode (a :< as) = (encode a, encode as)
-  decode (b,bs) = decode b :< decode bs
-
-instance Encodable a => Encodable (Tree Z a) where
-  EEncTy (Tree Z a, a)
-  encode (L a) = encode a
-  decode a = L (decode a)
-
-instance Encodable (Tree n a) => Encodable (Tree (S n) a) where
-  EEncTy (Tree (S n) a, Pair (Tree n a))
-  encode (B ts) = encode ts
-  decode x = B (decode x)
+RepEncode (Tree Z a, a, toL, unL)
+RepEncode (Tree (S n) a, Pair (Tree n a), toB, unB)
