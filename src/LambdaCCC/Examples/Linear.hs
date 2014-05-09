a8de77533a6b47b372e73c69e6f77a5535eebf3c
Conal Elliott <conal@conal.net>
2014-05-09 13:21:12 -0700

    Tree option

--- src/LambdaCCC/Examples/Linear.hs
+++ src/LambdaCCC/Examples/Linear.hs
@@ -1,25 +1,45 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -Wall #-}
+
+-- Whether to use trees instead of vectors.
+-- Restricts to power-of-two, and gives fast fold for dot.
+#define UseTrees
 
 module LambdaCCC.Examples.Linear where
 
 import Prelude hiding (sum)
-import Data.Foldable (sum)
+import Data.Foldable (Foldable(..),sum)
 import Data.Traversable (sequenceA)
+import Control.Applicative (Applicative,liftA2)
 
+import TypeUnary.Nat
+#ifdef UseTrees
+import Circat.RTree
+#else
 import TypeUnary.Vec hiding (transpose)
+#endif
 
 infixl 7 <.>
 
+#ifdef UseTrees
+type V = Tree
+#else
+type V = Vec
+#endif
+
 -- | Dot product, i.e., sum of products.
-(<.>) :: (IsNat n, Num a) => Vec n a -> Vec n a -> a
-u <.> v = sum (u * v)
+(<.>) :: (Foldable f, Applicative f, Num a) => f a -> f a -> a
+u <.> v = sum (liftA2 (*) u v)
 {-# INLINE (<.>) #-}
 
+-- (<.>) :: (IsNat n, Num a) => V n a -> V n a -> a
+-- u <.> v = sum (u * v)
+
 -- | Linear map from a^m to a^n: Matrix with n rows and m columns
-type Lin m n a = Vec n (Vec m a)
+type Lin m n a = V n (V m a)
 
 -- | Apply a linear map: dot each row of p with v
-apply :: (IsNat m, Num a) => Lin m n a -> Vec m a -> Vec n a
+apply :: (IsNat m, Num a) => Lin m n a -> V m a -> V n a
 apply p v = fmap (<.> v) p
 {-# INLINE apply #-}
 
