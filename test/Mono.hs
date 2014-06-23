927cd488240201f99015b075f8dcedbfc0449cbb
Conal Elliott <conal@conal.net>
2014-06-23 16:01:19 -0700

    Monomorphization tests

--- test/Mono.hs
+++ test/Mono.hs
@@ -5,6 +5,8 @@
 {-# OPTIONS_GHC -fcontext-stack=38 #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
 {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
 {-# OPTIONS_GHC -fno-warn-unused-binds   #-} -- TEMP
 
@@ -20,7 +22,6 @@
 -- 
 --   hermit Mono.hs -v0 -opt=LambdaCCC.Monomorphize DoMono.hss
 --   
--- Remove the 'resume' to see intermediate Core.
 ----------------------------------------------------------------------
 
 module Mono where
@@ -35,15 +36,49 @@ import Data.Foldable (Foldable(..),sum)
 
 import TypeUnary.TyNat
 import TypeUnary.Nat (IsNat)
-import TypeUnary.Vec (Vec(..))
+-- import TypeUnary.Vec (Vec(..))
 
 import Circat.Pair (Pair(..))
 import Circat.RTree (Tree(..))
+
+#define BuildDictBug
+
+#ifdef BuildDictBug
+-- Bug workaround. Hopefully remove soon See
+-- <https://github.com/ku-fpg/hermit/issues/88#issuecomment-46869719> and later
+-- comment.
+import LambdaCCC.Encode (Encodable(..))
+#endif
 
 {--------------------------------------------------------------------
     Examples
 --------------------------------------------------------------------}
 
-sum4 :: Tree (S (S Z)) Int -> Int
-sum4 = sum
+-- sum4 :: Tree N4 Int -> Int
+-- sum4 = sum
 
+test :: Tree Z Int -> Int
+test = sum
+
+-- test :: Pair Bool -> Bool
+-- test (a :# b) = a || b
+
+-- test :: Bool -> Bool
+-- test = not
+
+-- test :: Bool
+-- test = False
+
+-- foo :: Encodable a => a -> Encode a
+-- foo = encode
+
+-- test :: Tree Z Int -> Int
+-- test = sum
+
+-- test = encode (sum :: Tree Z Int -> Int)
+
+-- test = encode (L 3 :: Tree Z Int)
+
+-- -- test :: Tree Z Int -> Int
+-- test :: Tree Z Int -> Encode (Tree Z Int)
+-- test = encode
