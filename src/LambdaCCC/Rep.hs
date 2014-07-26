29fc78d97dec048344627e287287bcfad5851cfb
Conal Elliott <conal@conal.net>
2014-07-26 18:28:07 -0400

    equality constraint

--- src/LambdaCCC/Rep.hs
+++ src/LambdaCCC/Rep.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TypeFamilies, TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -Wall #-}
 
 {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
@@ -26,25 +27,48 @@ import TypeUnary.Vec (Vec(..),unConsV)
 import Circat.Pair (Pair(..),toP,fromP)
 import Circat.RTree (Tree(..),toL,unL,toB,unB)
 
+import Circat.Category (Rep)
+
 -- TODO: explicit exports
 
+#if 0
 class HasRep a where
-  type Rep a
   repr :: a -> Rep a
   abst :: Rep a -> a
+#else
+class HasRep a where
+  repr :: a' ~ Rep a => a -> a'
+  abst :: a' ~ Rep a => a' -> a
 
+-- More convenient construction:
+repr' :: HasRep a => a -> Rep a
+repr' = repr
+abst' :: HasRep a => Rep a -> a
+abst' = abst
+#endif
+
+type instance Rep (Pair a) = a :* a
 instance HasRep (Pair a) where
-  type Rep (Pair a) = a :* a
   repr (a :# a') = (a,a')
   abst (a,a') = (a :# a')
 
+type instance Rep (Vec Z a) = ()
+instance HasRep (Vec Z a) where
+  repr ZVec = ()
+  abst () = ZVec
+
+type instance Rep (Vec (S n) a) = a :* Vec n a
+instance HasRep (Vec (S n) a) where
+  repr (a :< as) = (a, as)
+  abst (a, as) = (a :< as)
+
+type instance Rep (Tree Z a) = a
 instance HasRep (Tree Z a) where
-  type Rep (Tree Z a) = a
   repr (L a) = a
   abst a = L a
 
+type instance Rep (Tree (S n) a) = Pair (Tree n a)
 instance HasRep (Tree (S n) a) where
-  type Rep (Tree (S n) a) = Pair (Tree n a)
   repr (B ts) = ts
   abst ts = B ts
 
