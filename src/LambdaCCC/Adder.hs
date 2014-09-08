73d65e604dbe1e2868739211d4d6a0923d268b63
Conal Elliott <conal@conal.net>
2014-09-08 16:23:11 -0700

    scans, State, StateTrie

--- src/LambdaCCC/Adder.hs
+++ src/LambdaCCC/Adder.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE TypeOperators, TypeFamilies, ViewPatterns, CPP #-}
+{-# LANGUAGE TypeOperators, TypeFamilies, ViewPatterns, TupleSections, CPP #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall #-}
 
 -- {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
@@ -15,23 +16,119 @@
 -- Scan-based adder
 ----------------------------------------------------------------------
 
+#define Testing
+
 module LambdaCCC.Adder where
 
 -- TODO: explicit exports
 
+import Prelude hiding (mapM)
+
 import Data.Monoid (Monoid(..))
 import Control.Applicative (Applicative,liftA2,(<$>))
+import Data.Traversable (Traversable,mapM)
 
-import Circat.Misc ((:*))
+-- import Control.Monad.Trans.State
+import Control.Monad.State (MonadState(..),runState) -- mtl
+
+#ifdef Testing
+import TypeUnary.TyNat (N2,N4)
+import TypeUnary.Vec (Vec,vec4)
+
+import Circat.Misc (transpose)
+import Circat.RTree (RTree)
+import qualified Circat.RTree as RT
+#endif
+
 import Circat.Rep
 import Circat.Scan
 import Circat.Pair
+import Circat.Shift (accumL)
 import Circat.Circuit (GenBuses(..),genBusesRep')
 
+import LambdaCCC.Misc ((:*))
 import LambdaCCC.Prim (xor)
+import LambdaCCC.StateTrie
+
+type Adder t = t (Pair Bool) -> t Bool :* Bool
+
+type Adder' t = Bool :* t (Pair Bool) -> t Bool :* Bool
+
+{--------------------------------------------------------------------
+    One-bit adders
+--------------------------------------------------------------------}
+
+halfAdd :: Pair Bool -> Bool :* Bool
+halfAdd (a :# b) = (a `xor` b,a && b)
+{-# INLINE halfAdd #-}
+
+add1 :: Bool :* Pair Bool -> Bool :* Bool
+add1 (ci, a :# b) = (s,co)
+ where
+   q  = a `xor` b
+   s  = q `xor` ci
+   co = (a && b) || (ci && q)
+{-# INLINE add1 #-}
+
+add1' :: Bool :* Pair Bool -> Bool :* Bool
+add1' (ci, a :# b) = (s',co || co')
+ where
+   (s ,co ) = halfAdd (a :# b)
+   (s',co') = halfAdd (s :# ci)
+{-# INLINE add1' #-}
+
+-- accumL :: Traversable t => (a :* b -> c :* a) -> (a :* t b -> t c :* a)
+
+{--------------------------------------------------------------------
+    mapM
+--------------------------------------------------------------------}
+
+add1State :: MonadState Bool m => Pair Bool -> m Bool
+add1State p = state (flip (curry add1) p)
+{-# INLINE add1State #-}
+
+adderSt :: (MonadState Bool m, Traversable t) =>
+           (m (t Bool) -> Bool -> (t Bool, Bool)) -> Adder' t
+adderSt run (ci,ps) = run (mapM add1State ps) ci
+{-# INLINE adderSt #-}
+
+adderState :: Traversable t => Adder' t
+adderState = adderSt runState
+{-# INLINE adderState #-}
+
+adderStateTrie :: Traversable t => Adder' t
+adderStateTrie = adderSt runStateTrie
+{-# INLINE adderStateTrie #-}
+
+{--------------------------------------------------------------------
+    traverse-based
+--------------------------------------------------------------------}
+
+-- type Adder' t = Bool :* t (Pair Bool) -> t Bool :* Bool
+
+-- sequenceA :: (Traversable t, Applicative f) => t (f a) -> f (t a)
+-- traverse  :: (Traversable t, Applicative f) => (a -> f b) -> t a -> f (t b)
+
+
+{--------------------------------------------------------------------
+    accum-based
+--------------------------------------------------------------------}
+
+adderAccumL :: Traversable t => Adder' t
+adderAccumL = accumL add1
+
+{--------------------------------------------------------------------
+    Scan-based
+--------------------------------------------------------------------}
 
 -- | Generate and propagate carries
 data GenProp = GenProp Bool Bool 
+
+gpGen :: GenProp -> Bool
+gpGen (GenProp _ g) = g
+
+gpProp :: GenProp -> Bool
+gpProp (GenProp p _) = p
 
 type instance Rep GenProp = Bool :* Bool
 instance HasRep GenProp where
@@ -51,33 +148,79 @@ genProp (a :# b) = GenProp (a && b) (a `xor` b)
 {-# INLINE gpProp #-}
 
 gpCarry :: GenProp -> Bool -> Bool
-gpCarry (GenProp g p) cin = g || cin && p  -- consolidate with mappend
+gpCarry (GenProp g p) cin = g || cin && p -- TODO: consolidate with mappend
 {-# INLINE gpCarry #-}
 
-type Adder t = t (Pair Bool) -> t Bool :* Bool
-
 scanAdd :: (Applicative t, LScan t) => Adder t
-scanAdd ps = (liftA2 h gprs cs, co)
+scanAdd ts = (liftA2 h gprs cs, co)
  where
-   gprs = genProp <$> ps
-   (fmap gpGen -> cs, gpGen -> co) = lscan gprs
+   gprs = genProp <$> ts
+   (mapl gpGen -> (cs,co)) = lscan gprs
    h (GenProp _ p) ci = p `xor` ci
 {-# INLINE scanAdd #-}
 
-type Adder' t = Bool :* t (Pair Bool) -> t Bool :* Bool
-
 scanAdd' :: (Applicative t, LScan t) => Adder' t
-scanAdd' (ci,ps) = (liftA2 h gprs' gprs, gpCarry gpo ci)
+scanAdd' (ci0,ts) = (liftA2 h gprs cs, co)
  where
-   gprs = genProp <$> ps
-   (gprs', gpo) = lscan gprs
-   h gpr' (GenProp _ p) = p `xor` gpCarry gpr' ci
+   gprs = genProp <$> ts
+   (mapl (flip gpCarry ci0) -> (cs,co)) = lscan gprs
+   h (GenProp _ p) ci = p `xor` ci
 {-# INLINE scanAdd' #-}
 
-gpGen :: GenProp -> Bool
-gpGen (GenProp _ g) = g
+-- scanAdd via scanAdd'
+scanAdd'' :: (Applicative t, LScan t) => Adder t
+scanAdd'' = scanAdd' . (False,)
 
-gpProp :: GenProp -> Bool
-gpProp (GenProp p _) = p
 
 instance GenBuses GenProp where genBuses' = genBusesRep'
+
+
+-- Handy operations
+
+(<$*>), mapr :: Functor t => (a -> b) -> (a :* t a) -> (b :* t b)
+f <$*> (a,as) = (f a, f <$> as)
+mapr = (<$*>)
+
+(<*$>), mapl :: Functor t => (a -> b) -> (t a :* a) -> (t b :* b)
+f <*$> (as,a) = (f <$> as, f a)
+mapl = (<*$>)
+
+#ifdef Testing
+
+{--------------------------------------------------------------------
+    Testing addition
+--------------------------------------------------------------------}
+
+v4a, v4b :: Vec N4 Bool
+v4a = vec4 True False True True
+v4b = vec4 True True False True
+
+rt2a, rt2b :: RTree N2 Bool
+rt2a = RT.tree2 True False True True
+rt2b = RT.tree2 True True False True
+
+-- type Adder' t = Bool :* t (Pair Bool) -> t Bool :* Bool
+
+-- (elemsV [False,False,False,True],True)
+addStateV4F :: Vec N4 Bool :* Bool
+addStateV4F = adderState (False,transpose (v4a :# v4b))
+
+-- (elemsV [True,False,False,True],True)
+addStateV4T :: Vec N4 Bool :* Bool
+addStateV4T = adderState (True,transpose (v4a :# v4b))
+
+addStateTrieV4F :: Vec N4 Bool :* Bool
+addStateTrieV4F = adderStateTrie (False,transpose (v4a :# v4b))
+
+addStateTrieV4T :: Vec N4 Bool :* Bool
+addStateTrieV4T = adderStateTrie (True,transpose (v4a :# v4b))
+
+-- (elemsV [False,False,False,True],True)
+scanAddV4F :: RTree N2 Bool :* Bool
+scanAddV4F = scanAdd' (False,transpose (rt2a :# rt2b))
+
+-- (elemsV [True,False,False,True],True)
+scanAddV4T :: RTree N2 Bool :* Bool
+scanAddV4T = scanAdd' (True,transpose (rt2a :# rt2b))
+
+#endif
