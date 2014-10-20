d1410504ce9b999a651f536d3508a8ecd8f2a007
Conal Elliott <conal@conal.net>
2014-10-20 11:38:00 -0700

    Use go, go', goM, goM' from Run. Explicit unfolds in *.hss.

--- src/LambdaCCC/Run.hs
+++ src/LambdaCCC/Run.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE TypeOperators, ConstraintKinds, FlexibleContexts #-}
+{-# LANGUAGE TypeOperators, ExistentialQuantification, FlexibleContexts #-}
+{-# LANGUAGE ConstraintKinds, StandaloneDeriving #-}
 {-# LANGUAGE CPP #-}
 
 {-# OPTIONS_GHC -Wall #-}
@@ -17,20 +18,25 @@
 -- Run a test: reify, CCC, circuit
 ----------------------------------------------------------------------
 
-module LambdaCCC.Run (go,run) where
+module LambdaCCC.Run (go,go',run,goM,goM') where
 
 import Prelude
 
 import LambdaCCC.Misc (Unop)
 import LambdaCCC.Lambda (EP,reifyEP)
 -- import LambdaCCC.CCC ((:->),convertC)
-import LambdaCCC.ToCCC (toCCC')
+import LambdaCCC.ToCCC (toCCC',toCCC)
 
-import Circat.Circuit (GenBuses,(:>),Attr,mkGraph,unitize,outDotG)
+import Circat.Circuit (GenBuses,(:>),Attr,mkGraph,unitize,UU,outDotG,MealyC(..),unitizeMealyC)
 import Circat.Netlist (saveAsVerilog)
+import Circat.Mealy (Mealy(..))
+
+go' :: GenBuses a => String -> [Attr] -> (a -> b) -> IO ()
+go' name attrs f = run name attrs (reifyEP f)
+{-# INLINE go' #-}
 
 go :: GenBuses a => String -> (a -> b) -> IO ()
-go name f = run name [] (reifyEP f)
+go name = go' name []
 {-# INLINE go #-}
 
 -- Broken:
@@ -38,38 +44,55 @@ go name f = run name [] (reifyEP f)
 
 -- Run an example: reify, CCC, circuit.
 run :: GenBuses a => String -> [Attr] -> EP (a -> b) -> IO ()
-#ifdef ViaTerm
-run name attrs expr = do 
---                          putStrLn "Generating circuit"
---                          print expr
---                          print term
-                         outGV name attrs circ
- where
-   term = toCCC' expr
-   circ = convertC term
-#else
-run name attrs e = do 
---                       putStrLn "Generating circuit"
-                      print e
---                       print (idCT (toCCC' e))
-                      outGV name attrs (toCCC' e)
-
--- -- Identity on CCC terms
--- idCT :: Unop (a :-> b)
--- idCT = id
-
-#endif
+run name attrs e = do print e
+                      outGV name attrs (unitize (toCCC' e))
+{-# NOINLINE run #-}
 
 -- Diagram and Verilog
-outGV :: GenBuses a => String -> [Attr] -> (a :> b) -> IO ()
+outGV :: String -> [Attr] -> UU -> IO ()
 outGV name attrs circ =
   do outD ("pdf","")
      -- outD ("svg","") 
      -- outD ("png","-Gdpi=200")
      outV
  where
-   g       = mkGraph name (unitize circ)
+   g       = mkGraph name circ
    outD ss = outDotG ss attrs g
    outV    = saveAsVerilog g
+{-# NOINLINE outGV #-}
 
 -- TODO: Move file-saving code from outD and saveVerilog to here.
+
+
+{--------------------------------------------------------------------
+    State machines
+--------------------------------------------------------------------}
+
+data MealyE a b =
+  forall s. GenBuses s => MealyE (EP ((s,a) -> (s,b))) (EP s)
+
+deriving instance Show (MealyE a b)
+
+reifyMealy :: Mealy a b -> MealyE a b
+reifyMealy (Mealy f s) = MealyE (reifyEP f) (reifyEP s)
+{-# INLINE reifyMealy #-}
+
+toMealyC :: MealyE a b -> MealyC a b
+toMealyC (MealyE f s) = MealyC (toCCC' f) (toCCC s)
+{-# NOINLINE toMealyC #-}
+
+runM :: GenBuses a => String -> [Attr] -> MealyE a b -> IO ()
+runM name attrs e = do print e
+                       outGV name attrs (unitizeMealyC (toMealyC e))
+{-# NOINLINE runM #-}
+
+goM :: GenBuses a => String -> Mealy a b -> IO ()
+goM name = goM' name []
+{-# INLINE goM #-}
+
+goM' :: GenBuses a => String -> [Attr] -> Mealy a b -> IO ()
+goM' name attrs m = runM name attrs (reifyMealy m)
+{-# INLINE goM' #-}
+
+-- TODO: Maybe pull unitizeMealyC into toMealyC, renaming to "toMealyU"
+
