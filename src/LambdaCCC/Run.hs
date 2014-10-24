76cccc6881870d1220f07c393ab0466e769eef45
Conal Elliott <conal@conal.net>
2014-10-24 11:35:15 -0700

    tightened: no more MealyToArrow

--- src/LambdaCCC/Run.hs
+++ src/LambdaCCC/Run.hs
@@ -18,25 +18,16 @@
 -- Run a test: reify, CCC, circuit
 ----------------------------------------------------------------------
 
--- #define MealyToArrow
-
 module LambdaCCC.Run (go,go',run,goM,goM') where
 
 import Prelude
 import Control.Arrow (first)
 
 import LambdaCCC.Lambda (EP,reifyEP)
--- import LambdaCCC.CCC ((:->),convertC)
 import LambdaCCC.ToCCC (toCCC')
 
 import Circat.Circuit
-  (GenBuses,Attr,mkGraph,unitize,UU,outDotG,MealyC(..)
-#ifdef MealyToArrow
-  , mealyAsArrow, unitize
-#else
-  , unitizeMealyC
-#endif
-  )
+  (GenBuses,Attr,mkGraph,unitize,UU,outDotG,MealyC(..),unitizeMealyC)
 import Circat.Netlist (saveAsVerilog)
 import Circat.Mealy (Mealy(..))
 
@@ -47,9 +38,6 @@ go' name attrs f = goM' name attrs (Mealy (first f) ())
 go :: GenBuses a => String -> (a -> b) -> IO ()
 go name = go' name []
 {-# INLINE go #-}
-
--- Broken:
--- #define ViaTerm
 
 -- Run an example: reify, CCC, circuit.
 run :: GenBuses a => String -> [Attr] -> EP (a -> b) -> IO ()
@@ -76,37 +64,13 @@ outGV name attrs circ =
     State machines
 --------------------------------------------------------------------}
 
+-- Reified Mealy machine
 data MealyE a b =
   forall s. (GenBuses s, Show s) => MealyE (EP ((a,s) -> (b,s))) s
 
+-- The Show constraint is just for the following Show, which is handy for debugging.
+-- (See the 'print' in toMealyC.)
 deriving instance Show (MealyE a b)
-
-#ifdef MealyToArrow
-
-reifyMealy :: Mealy a b -> MealyE a b
-reifyMealy (Mealy f s) = MealyE (reifyEP f) s
-{-# INLINE reifyMealy #-}
-
-toMealyC :: MealyE a b -> MealyC a b
-toMealyC (MealyE f s) = MealyC (toCCC' f) s
-
-runM :: GenBuses a => String -> [Attr] -> MealyE a b -> IO ()
-runM name attrs e = do print e
-                       outGV name attrs (unitize (mealyAsArrow (toMealyC e)))
-
--- TODO: Change outGV to take (a :> b) in place of UU, and drop unitizeC here.
-
-goM :: GenBuses a => String -> Mealy a b -> IO ()
-goM name = goM' name []
-{-# INLINE goM #-}
-
-goM' :: GenBuses a => String -> [Attr] -> Mealy a b -> IO ()
-goM' name attrs m = runM name attrs (reifyMealy m)
-{-# INLINE goM' #-}
-
--- TODO: Rework goM' via go' instead of vice versa
-
-#else
 
 reifyMealy :: Mealy a b -> MealyE a b
 reifyMealy (Mealy f s) = MealyE (reifyEP f) s
@@ -127,12 +91,11 @@ goM' :: GenBuses a => String -> [Attr] -> Mealy a b -> IO ()
 goM' name attrs m = runM name attrs (reifyMealy m)
 {-# INLINE goM' #-}
 
-#endif
+-- TODO: When mealyAsArrow works, rewrite goM' via go' instead of vice versa
 
 -- Despite INLINE pragmas, I still have to explicitly tell HERMIT to unfold
 -- definitions from this module:
 -- 
 -- try (any-td (unfold ['go,'go','goM,'goM','reifyMealy]))
-
 
 -- TODO: Maybe pull unitizeMealyC into toMealyC, renaming to "toMealyU"
