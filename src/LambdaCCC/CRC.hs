6e13d9cd800408fd9f690d6a9ec05de455ae42e0
Conal Elliott <conal@conal.net>
2014-11-08 15:10:34 -0800

    crcS

--- src/LambdaCCC/CRC.hs
+++ src/LambdaCCC/CRC.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE TypeOperators, ViewPatterns #-}
+{-# LANGUAGE TypeOperators, ViewPatterns, ConstraintKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wall #-}
 
 -- {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
@@ -20,18 +21,17 @@ module LambdaCCC.CRC where
 
 -- TODO: explicit exports
 
-import Prelude hiding (foldl)
+import Prelude hiding (foldl,scanl,sum)
 
 import Control.Applicative -- (Applicative(..),liftA2,liftA3)
-import Data.Foldable (Foldable(..))
+import Data.Foldable (Foldable(..),sum)
 import Data.Traversable (Traversable(..))
 -- import Control.Category (id,(.))
 
 import LambdaCCC.Misc (xor,(:*))
-
 import Circat.Shift
--- import Circat.Mealy hiding (sumS)
--- import qualified Circat.Mealy as Mealy
+import Circat.Mealy (Mealy(..))  -- ,scanl
+import Circat.Circuit (GenBuses)
 
 crcStep :: (Traversable poly, Applicative poly) =>
            poly Bool -> poly Bool :* Bool -> poly Bool
@@ -64,6 +64,8 @@ crc poly = foldlQ (crcStep poly) . shiftRF
 foldlQ :: Foldable f => (b :* a -> b) -> (b :* f a -> b)
 foldlQ = uncurry . foldl . curry
 
+#if 0
+
 -- Equivalently,
 --
 -- crc poly (shiftRF -> (seg',msg')) = foldlQ (crcStep poly) (seg',msg')
@@ -86,3 +88,47 @@ crc' :: (Traversable poly, Applicative poly, Traversable msg) =>
 crc' poly msg pad = foldl (crcStep' poly) seg0 msg0
  where
    (seg0,msg0) = shiftRF (msg,pad)
+
+#endif
+
+type MealyFun s a b = (a,s) -> (b,s)
+
+-- Given an input bit,
+
+-- *   If $i < p$, shift the input bit into the polynomial.
+-- *   If $p \le i < 2 p$, shift into the remainder.
+-- *   If $2 p \le i$, do a CRC step, updating the remainder.
+
+#if 1
+
+-- Serial
+crcS :: forall poly. (GenBuses (poly Bool), Show (poly Bool), Applicative poly, Traversable poly) =>
+        Mealy Bool (poly Bool, Int)
+crcS = Mealy h (pure False, pure False,0)
+ where
+   p = sizeAF (undefined :: poly Bool)
+   h :: MealyFun (poly Bool, poly Bool, Int) Bool (poly Bool, Int)
+   h (b,(poly,seg,i)) = ((seg',i'),(poly',seg',i'))
+    where
+      i' = i + 1
+      stash q = snd (shiftR (q,b))
+      (poly',seg')
+        | i < p     = (stash poly,seg)
+        | i < 2*p   = (poly,stash seg)
+        | otherwise = (poly,crcStep poly (seg,b))
+{-# INLINE crcS #-}
+
+-- TODO: rewrite via scanl
+
+-- Size of a structure without looking at the structure
+sizeAF :: forall f a. (Applicative f, Foldable f) => f a -> Int
+sizeAF _ = sum (pure 1 :: f Int)
+
+-- Consider record update
+
+-- scanl :: C b => (b -> a -> b) -> b -> Mealy a b
+
+-- crcStep :: (Traversable poly, Applicative poly) =>
+--            poly Bool -> poly Bool :* Bool -> poly Bool
+
+#endif
