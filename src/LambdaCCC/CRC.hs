bf2339cead686d237631124d7d5ee3a95a79a6c5
Conal Elliott <conal@conal.net>
2014-11-16 23:14:27 -0800

    misc

--- src/LambdaCCC/CRC.hs
+++ src/LambdaCCC/CRC.hs
@@ -28,10 +28,14 @@ import Data.Foldable (Foldable(..),sum)
 import Data.Traversable (Traversable(..))
 -- import Control.Category (id,(.))
 
-import LambdaCCC.Misc (xor,(:*))
+import LambdaCCC.Misc (xor,(:*)) -- ,dup
 import Circat.Shift
-import Circat.Mealy (Mealy(..))  -- ,scanl
+import Circat.Mealy (Mealy(..),scanl)
 import Circat.Circuit (GenBuses)
+
+-- TEMP
+import TypeUnary.Vec hiding (transpose)
+import Circat.RTree
 
 crcStep :: (Traversable poly, Applicative poly) =>
            poly Bool -> poly Bool :* Bool -> poly Bool
@@ -103,12 +107,12 @@ type MealyFun s a b = (a,s) -> (b,s)
 
 -- Serial
 crcS :: forall poly. (GenBuses (poly Bool), Show (poly Bool), Applicative poly, Traversable poly) =>
-        Mealy Bool (poly Bool, Int)
+        Mealy Bool (poly Bool)
 crcS = Mealy h (pure False, pure False,0)
  where
-   p = sizeAF (undefined :: poly Bool)
-   h :: MealyFun (poly Bool, poly Bool, Int) Bool (poly Bool, Int)
-   h (b,(poly,seg,i)) = ((seg',i'),(poly',seg',i'))
+   p = sizeA (undefined :: poly Bool)
+   h :: MealyFun (poly Bool, poly Bool, Int) Bool (poly Bool)
+   h (b,(poly,seg,i)) = (seg',(poly',seg',i'))
     where
       i' = i + 1
       stash q = snd (shiftR (q,b))
@@ -121,8 +125,8 @@ crcS = Mealy h (pure False, pure False,0)
 -- TODO: rewrite via scanl
 
 -- Size of a structure without looking at the structure
-sizeAF :: forall f a. (Applicative f, Foldable f) => f a -> Int
-sizeAF _ = sum (pure 1 :: f Int)
+sizeA :: forall f a. (Applicative f, Foldable f) => f a -> Int
+sizeA _ = sum (pure 1 :: f Int)
 
 -- Consider record update
 
@@ -131,4 +135,75 @@ sizeAF _ = sum (pure 1 :: f Int)
 -- crcStep :: (Traversable poly, Applicative poly) =>
 --            poly Bool -> poly Bool :* Bool -> poly Bool
 
+-- -- Serial with static polynomial
+-- crcSK :: forall poly. ( GenBuses (poly Bool), Show (poly Bool)
+--                       , Applicative poly, Traversable poly ) =>
+--          poly Bool -> Mealy Bool (poly Bool)
+-- crcSK poly = Mealy h (pure False,0)
+--  where
+--    p = sizeA (undefined :: poly Bool)
+--    h :: MealyFun (poly Bool, Int) Bool (poly Bool)
+--    h (b,(seg,i)) = (seg',(seg',i'))
+--     where
+--       -- This version doesn't increment i past p, to prevent overflow.
+--       (seg',i') | i < p     = (snd (shiftR (seg,b)), i+1)
+--                 | otherwise = (crcStep poly (seg,b), i)
+-- {-# INLINE crcSK #-}
+
+--       i' = i + 1
+--       seg' | i < p     = snd (shiftR (seg,b))
+--            | otherwise = crcStep poly (seg,b)
+
+-- Serial with static polynomial
+crcSK2 :: forall poly. ( GenBuses (poly Bool), Show (poly Bool)
+                       , Applicative poly, Traversable poly ) =>
+          poly Bool -> Mealy Bool (poly Bool)
+crcSK2 poly = fst <$> scanl h (pure False,0)
+ where
+   p = sizeA (undefined :: poly Bool)
+   -- h :: (poly Bool, Int) -> Bool -> (poly Bool, Int)
+   h (seg,i) b | i < p     = (snd (shiftR (seg,b)), i+1)
+               | otherwise = (crcStep poly (seg,b), i)
+{-# INLINE crcSK2 #-}
+
+-- scanl :: C b => (b -> a -> b) -> b -> Mealy a b
+
 #endif
+
+{--------------------------------------------------------------------
+    Sample input
+--------------------------------------------------------------------}
+
+-- I'm putting these definitions here to work around a problem with inlining.
+-- Fix that problem, and move these definitions back to TreeTest.hs.
+
+polyV1 :: Vec N1 Bool
+polyV1 = vec1 True
+
+polyV2 :: Vec N2 Bool
+polyV2 = vec2 True False
+
+polyV3 :: Vec N3 Bool
+polyV3 = vec3 True False True
+
+polyV4 :: Vec N4 Bool
+polyV4 = vec4 True False False True
+
+polyV5 :: Vec N5 Bool
+polyV5 = polyV3 <+> polyV2
+
+polyRT0 :: Tree N0 Bool
+polyRT0 = tree0 True
+
+polyRT1 :: Tree N1 Bool
+polyRT1 = tree1 True False
+
+polyRT2 :: Tree N2 Bool
+polyRT2 = tree2 True False False True
+
+polyRT3 :: Tree N3 Bool
+polyRT3 = tree3 True False False True True False True False
+
+polyRT4 :: Tree N4 Bool
+polyRT4 = tree4 True False False True True False True False
+                False True True False True False True False
