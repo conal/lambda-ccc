50e38e98f910366061e99bd1801023cf00e323f1
David Banas <capn.freako@gmail.com>
2015-10-03 16:33:26 -0700

    fft_test.hs almost working.
    First and last output elements appear to be swapped, in some cases.

--- test/fft_test.hs
+++ test/fft_test.hs
@@ -16,13 +16,13 @@ module Main where
 import Prelude hiding ({- id,(.), -}foldl,foldr,sum,product,zipWith,reverse,and,or,scanl,minimum,maximum)
 
 import Data.Complex
-import Data.Foldable (Foldable(..),sum,product,and,or,toList,minimum,maximum)
+import Data.Foldable (sum)
 
-import TypeUnary.TyNat
 import TypeUnary.Nat (IsNat,natToZ,Nat(..),nat)
 import Circat.Pair (Pair(..))
 import qualified Circat.Pair as P
 import qualified Circat.RTree as RT
+import Circat.Misc (transpose)
 
 type RTree = RT.Tree
 
@@ -34,12 +34,7 @@ instance Eq a => Ord (Complex a) where
 
 -- A 2-element FFT.
 fft_2 :: Num a => Pair a -> Pair a
-fft_2 p = P.toP (sum p, sum p')
-    where p' = P.toP (x, -y)
-          x  = P.fstP p
-          y  = P.sndP p
--- fft_2 p = P.toP (sum p', sum (secondP (-) p'))
---   where p' = secondP addPhasors p
+fft_2 p = P.toP (sum p, sum (P.secondP (0 -) p))
 
 addPhase :: Nat n -> Complex Double -> Complex Double
 addPhase Zero = id
@@ -49,9 +44,20 @@ fft_r2_dit :: IsNat n => RTree n (Complex Double) -> RTree n (Complex Double)
 fft_r2_dit = fft_r2_dit' nat
 
 fft_r2_dit' :: IsNat n => Nat n -> RTree n (Complex Double) -> RTree n (Complex Double)
-fft_r2_dit' Zero = RT.butterfly fft_2
-fft_r2_dit' n    = RT.butterfly (fft_2 . (P.secondP (addPhase n)))
+fft_r2_dit' Zero        = error "Whoops! Shouldn't have ended up here."
+fft_r2_dit' (Succ Zero) = RT.butterfly fft_2
+fft_r2_dit' (Succ n)    = RT.butterfly (fft_2 . (P.secondP (addPhase n)))
 
 main :: IO ()
-main = putStrLn "Hello, World!"
+-- Delta - Should produce: [1, 1, 1, 1] as output. Works.
+-- main = print $ fft_r2_dit $ RT.tree2 (1.0 :+ 0.0) (0.0 :+ 0.0) (0.0 :+ 0.0) (0.0 :+ 0.0)
+
+-- Constant - Should produce [4, 0, 0, 0] as output. First and last output elements seem to be swapped.
+-- main = print $ fft_r2_dit $ RT.tree2 (1.0 :+ 0.0) (1.0 :+ 0.0) (1.0 :+ 0.0) (1.0 :+ 0.0)
+
+-- Nyquist - Should produce [0, 0, 4, 0] as output. Works.
+-- main = print $ fft_r2_dit $ RT.tree2 (1.0 :+ 0.0) ((-1.0) :+ 0.0) (1.0 :+ 0.0) ((-1.0) :+ 0.0)
+
+-- Fundamental - Should produce [0, 2, 0, 2] as output. First and last output elements seem to be swapped.
+main = print $ fft_r2_dit $ RT.tree2 (1.0 :+ 0.0) (0.0 :+ 0.0) ((-1.0) :+ 0.0) (0.0 :+ 0.0)
 
