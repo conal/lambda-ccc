ceea07971eb0126a44a4675b99e5d29f224d7acc
David Banas <capn.freako@gmail.com>
2015-10-04 07:55:34 -0700

    Added DFT reference and PrettyDouble, for easier viewing.
    Here is the current output:
    
      :!./fft_test
    
      Testing input: [ 1.000 :+  0.000, 0.000 :+  0.000, 0.000 :+  0.000,
      0.000 :+  0.000]
      Expected output: [ 1.000 :+  0.000, 1.000 :+  0.000, 1.000 :+  0.000,
      1.000 :+  0.000]
      Actual output:   B (B (L ( 1.000 :+  0.000) :# L ( 1.000 :+  0.000)) :#
      B (L ( 1.000 :+  0.000) :# L ( 1.000 :+  0.000)))
    
      Testing input: [ 1.000 :+  0.000, 1.000 :+  0.000, 1.000 :+  0.000,
      1.000 :+  0.000]
      Expected output: [ 4.000 :+  0.000, 0.000 :+  0.000, 0.000 :+  0.000,
      0.000 :+  0.000]
      Actual output:   B (B (L ( 0.000 :+  0.000) :# L ( 0.000 :+  0.000)) :#
      B (L ( 0.000 :+  0.000) :# L ( 4.000 :+  0.000)))
    
      Testing input: [ 1.000 :+  0.000,-1.000 :+  0.000, 1.000 :+
      0.000,-1.000 :+  0.000]
      Expected output: [ 0.000 :+  0.000, 0.000 :+  0.000, 4.000 :+  0.000,
      0.000 :+  0.000]
      Actual output:   B (B (L ( 0.000 :+  0.000) :# L ( 0.000 :+  0.000)) :#
      B (L ( 4.000 :+  0.000) :# L ( 0.000 :+  0.000)))
    
      Testing input: [ 1.000 :+  0.000, 0.000 :+  0.000,-1.000 :+  0.000,
      0.000 :+  0.000]
      Expected output: [ 0.000 :+  0.000, 2.000 :+  0.000, 0.000 :+  0.000,
      2.000 :+  0.000]
      Actual output:   B (B (L ( 2.000 :+  0.000) :# L ( 2.000 :+  0.000)) :#
      B (L ( 0.000 :+  0.000) :# L ( 0.000 :+  0.000)))
    
      Testing input: [ 0.000 :+  0.000, 1.000 :+  0.000, 0.000 :+
      0.000,-1.000 :+  0.000]
      Expected output: [ 0.000 :+  0.000, 0.000 :+ -2.000, 0.000 :+  0.000,
      0.000 :+  2.000]
      Actual output:   B (B (L (-2.000 :+  0.000) :# L ( 2.000 :+  0.000)) :#
      B (L ( 0.000 :+  0.000) :# L ( 0.000 :+  0.000)))
    
    As can be seen, above, there is more wrong than just a swap of the first
    and last output elements. The last case, for instance, is completely
    missing the 90-deg. phase shift, in both non-zero output elements.

--- test/fft_test.hs
+++ test/fft_test.hs
@@ -15,14 +15,15 @@ module Main where
 
 import Prelude hiding ({- id,(.), -}foldl,foldr,sum,product,zipWith,reverse,and,or,scanl,minimum,maximum)
 
+import Control.Monad (forM_)
 import Data.Complex
 import Data.Foldable (sum)
+import Data.Newtypes.PrettyDouble
 
 import TypeUnary.Nat (IsNat,natToZ,Nat(..),nat)
 import Circat.Pair (Pair(..))
 import qualified Circat.Pair as P
 import qualified Circat.RTree as RT
-import Circat.Misc (transpose)
 
 type RTree = RT.Tree
 
@@ -36,28 +37,54 @@ instance Eq a => Ord (Complex a) where
 fft_2 :: Num a => Pair a -> Pair a
 fft_2 p = P.toP (sum p, sum (P.secondP (0 -) p))
 
-addPhase :: Nat n -> Complex Double -> Complex Double
+-- Phasor, as a function of tree depth.
+addPhase :: Nat n -> Complex PrettyDouble -> Complex PrettyDouble
 addPhase Zero = id
 addPhase n    = (* (cis ((-pi) / (natToZ n))))
 
-fft_r2_dit :: IsNat n => RTree n (Complex Double) -> RTree n (Complex Double)
+-- Radix-2, DIT FFT
+fft_r2_dit :: IsNat n => RTree n (Complex PrettyDouble) -> RTree n (Complex PrettyDouble)
 fft_r2_dit = fft_r2_dit' nat
 
-fft_r2_dit' :: IsNat n => Nat n -> RTree n (Complex Double) -> RTree n (Complex Double)
+fft_r2_dit' :: IsNat n => Nat n -> RTree n (Complex PrettyDouble) -> RTree n (Complex PrettyDouble)
 fft_r2_dit' Zero        = error "Whoops! Shouldn't have ended up here."
 fft_r2_dit' (Succ Zero) = RT.butterfly fft_2
 fft_r2_dit' (Succ n)    = RT.butterfly (fft_2 . (P.secondP (addPhase n)))
+-- fft_r2_dit' n           = RT.butterfly (fft_2 . (P.secondP (addPhase n)))
+
+-- Test config.
+realData :: [[PrettyDouble]]
+realData = [  [1.0,   0.0,   0.0,   0.0 ]  -- Delta
+            , [1.0,   1.0,   1.0,   1.0 ]  -- Constant
+            , [1.0, (-1.0),  1.0, (-1.0)]  -- Nyquist
+            , [1.0,   0.0, (-1.0),  0.0 ]  -- Fundamental
+            , [0.0,   1.0,   0.0, (-1.0)]  -- Fundamental w/ 90-deg. phase lag
+           ]
+complexData :: [[Complex PrettyDouble]]
+complexData = map (map (:+ 0.0)) realData
+
+myTree2 [w, x, y, z] = RT.tree2 w x y z
+myTree2 _            = error "Something went horribly wrong!"
+
+-- Discrete Fourier Transform (DFT) (our "truth" reference)
+-- O(n^2)
+--
+dft :: RealFloat a => [Complex a] -> [Complex a]
+dft xs = [ sum [ x * exp((0.0 :+ (-1.0)) * 2 * pi / lenXs * fromIntegral(k * n))
+                 | (x, n) <- zip xs [0..]
+               ]
+           | k <- [0..(length xs - 1)]
+         ]
+    where lenXs = fromIntegral $ length xs
 
 main :: IO ()
--- Delta - Should produce: [1, 1, 1, 1] as output. Works.
--- main = print $ fft_r2_dit $ RT.tree2 (1.0 :+ 0.0) (0.0 :+ 0.0) (0.0 :+ 0.0) (0.0 :+ 0.0)
-
--- Constant - Should produce [4, 0, 0, 0] as output. First and last output elements seem to be swapped.
--- main = print $ fft_r2_dit $ RT.tree2 (1.0 :+ 0.0) (1.0 :+ 0.0) (1.0 :+ 0.0) (1.0 :+ 0.0)
-
--- Nyquist - Should produce [0, 0, 4, 0] as output. Works.
--- main = print $ fft_r2_dit $ RT.tree2 (1.0 :+ 0.0) ((-1.0) :+ 0.0) (1.0 :+ 0.0) ((-1.0) :+ 0.0)
-
--- Fundamental - Should produce [0, 2, 0, 2] as output. First and last output elements seem to be swapped.
-main = print $ fft_r2_dit $ RT.tree2 (1.0 :+ 0.0) (0.0 :+ 0.0) ((-1.0) :+ 0.0) (0.0 :+ 0.0)
+main = do
+    forM_ complexData (\x -> do
+        putStr "\nTesting input: "
+        print x
+        putStr "Expected output: "
+        print $ dft x
+        putStr "Actual output:   "
+        print $ fft_r2_dit $ myTree2 x
+        )
 
