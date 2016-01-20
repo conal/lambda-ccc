b36a9c71ccf91b29210329fdf7e131357bc8db11
Conal Elliott <conal@conal.net>
2016-01-20 00:42:16 -0800

    print name with lambda term in run

--- src/LambdaCCC/Run.hs
+++ src/LambdaCCC/Run.hs
@@ -67,7 +67,7 @@ goSep name s = go' name [ranksep s]
 
 -- Run an example: reify, CCC, circuit.
 run :: Okay a => String -> [Attr] -> EP a -> IO ()
-run name attrs e = do print e
+run name attrs e = do putStrLn (name ++ " = " ++ show e)
                       outGV name attrs (unitize' (toCCC e))
 {-# NOINLINE run #-}
 
